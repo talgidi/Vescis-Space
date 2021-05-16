d1aee785be6a875e00ea8cb6cf46491f7cbb8145
Rene Rivas <reinaldorrivas@gmail.com>
2021-05-16 16:22:10 -0400

    Reused Vescis Component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
   const vescisBlockData = {
     titleText: "This is an component text title!",
     paragraphText: "Dangelo didn't truly read de docs! Just kidding! =D",
-    sourceImage: "https://www.xnxx.com/",
+    sourceImage:
+      "https://i0.wp.com/wtfonline.mx/wp-content/uploads/2020/06/abella-danger-wtfonlinemx-1.jpg?fit=803%2C1024&ssl=1",
+  };
+  const vescisBlockDataAlt = {
+    titleText: "Titulo Secundario",
+    paragraphText: "Reutilizando Componente",
+    sourceImage:
+      "https://i0.wp.com/wtfonline.mx/wp-content/uploads/2020/06/abella-danger-wtfonlinemx-1.jpg?fit=803%2C1024&ssl=1",
   };
 
   return (
@@ -16,6 +23,7 @@ function App() {
       <Header />
       <Carrusel />
       <BloqueVescis data={vescisBlockData} />
+      <BloqueVescis data={vescisBlockDataAlt} />
     </React.Fragment>
   );
 }
